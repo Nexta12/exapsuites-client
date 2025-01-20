eee7fd6d1fb76e5aefe0c44a981ad18e878440ec
Ernest <ernestez12@gmail.com>
2025-01-20 07:02:56 +0100

    changed header test2

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -11,17 +11,17 @@ import { paths } from "@routes/paths";
 
 const slides = [
   {
-    title: "Your Luxury Apartments For Vacation",
+    title: "Your Luxury Apartments",
     bg: Img1,
     btnText: "See our rooms",
   },
   {
-    title: "Your Luxury Apartments For Vacation",
+    title: "Your Luxury Apartments",
     bg: Img2,
     btnText: "See our rooms",
   },
   {
-    title: "Your Luxury Apartments For Vacation",
+    title: "Your Luxury Apartments",
     bg: Img3,
     btnText: "See our rooms",
   },
