dad38515fe959f8ed2c1e6da2145c9b07e3acab8
Ernest <ernestez12@gmail.com>
2025-01-20 07:29:35 +0100

    addressed the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,10 @@ import { ExapLogo } from './Logo';
 const Footer = () => {
   return (
 
-    <footer className='bg-primary py-12' >
-         <div className="container mx-auto text-white flex justify-between">
+    <footer className='bg-primary py-12 ' >
+         <div className="container mx-auto text-white flex-col lg:flex-row gap-4 justify-between">
           {/* Logo */}
-          <Link to="/">
+          <Link to="/" className='mb-4'>
            <ExapLogo/>
           </Link>
           Copyright &copy; 2025. All rights reserved
